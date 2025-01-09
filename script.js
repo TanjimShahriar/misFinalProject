cb88acdbd82ecca3b91a96349e657d84cd0c5a27
TanjimShahriar <tanjimnabil@gmail.com>
2025-01-10 02:24:08 +0600

    7th

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ searchButton.addEventListener("click", function () {
   fetchMeals(query);
 });
 
+function fetchMeals(query) {
+    var API_URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+    fetch(API_URL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        if (data.meals) {
+          allResults = data.meals;
+          displayMeals(allResults.slice(0, 5), true);
+        } else {
+          resultsContainer.innerHTML = `<p>No meals found for "${query}".</p>`;
+        }
+      })
+      .catch(function (error) {
+        console.error("Error fetching meals:", error);
+        resultsContainer.innerHTML = `<p>Failed to fetch meals. Please try again later.</p>`;
+      });
+  }
